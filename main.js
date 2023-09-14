e8deae6b2427e3c65622f0407e5af39c8538a06f
Hamid Abdullahi <hamid.ab.dev@gmail.com>
2023-09-14 21:14:01 +0330

    I add the for loop and while and do while

--- main.js
+++ main.js
@@ -114,36 +114,54 @@
 // }
 
 //? Challenge
-const point = "5";
-switch (point) {
-  case "20":
-  case "19":
-  case "18": {
-    console.log("Very Good!");
-    break;
-  }
-  case "17":
-  case "16":
-  case "15":
-  case "14":
-  case "13":
-  case "12":
-  case "11":
-  case "10": {
-    console.log("Not Bad");
-    break;
-  }
-  case "9":
-  case "8":
-  case "7":
-  case "6":
-  case "5":
-  case "4":
-  case "3":
-  case "2":
-  case "1":
-  case "0": {
-    console.log("Very Bad!!!");
-    break;
-  }
-}
+// const point = "5";
+// switch (point) {
+//   case "20":
+//   case "19":
+//   case "18": {
+//     console.log("Very Good!");
+//     break;
+//   }
+//   case "17":
+//   case "16":
+//   case "15":
+//   case "14":
+//   case "13":
+//   case "12":
+//   case "11":
+//   case "10": {
+//     console.log("Not Bad");
+//     break;
+//   }
+//   case "9":
+//   case "8":
+//   case "7":
+//   case "6":
+//   case "5":
+//   case "4":
+//   case "3":
+//   case "2":
+//   case "1":
+//   case "0": {
+//     console.log("Very Bad!!!");
+//     break;
+//   }
+// }
+
+//! for loop
+// for (let i = 100; i >= 50; i--) {
+//   if (i % 2 === 0) console.log("The number is" + " " + i);
+// }
+
+//! while
+// let i = 100;
+// while (i >= 50) {
+//   if (i % 2 === 0) console.log("The number is", i);
+//   i--;
+// }
+//! while do
+// let i = 100;
+// do {
+//   if (i % 2 === 0) console.log("The number is", i);
+//   i--;
+// } while (i >= 50);
